91028450f72efbb9fd3defe45dfe43c2753b6c90
Patrik Jánosdeák  <patrik.janosdeak@dentsu.com>
2023-07-05 16:20:08 +0200

    Add logtail.flush to proxy

--- src/runtime/utilities/enableConsoleProxy.ts
+++ src/runtime/utilities/enableConsoleProxy.ts
@@ -63,21 +63,25 @@ const enableConsoleProxy: ConsoleProxyFunction = (logtail) => {
   console.log = (...args) => {
     legacyLog(...args)
     logtail.log(serializeConsoleLog(...args))
+    logtail.flush()
   }
 
   console.info = (...args) => {
     legacyInfo(...args)
     logtail.info(serializeConsoleLog(...args))
+    logtail.flush()
   }
 
   console.warn = (...args) => {
     legacyWarn(...args)
     logtail.warn(serializeConsoleLog(...args))
+    logtail.flush()
   }
 
   console.error = (...args) => {
     legacyError(...args)
     logtail.error(serializeConsoleLog(...args))
+    logtail.flush()
   }
 }
 
